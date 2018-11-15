1e92d08ea84266f5f4aabad6e193ec86c463c579
Justin Browne <jebdude89@gmail.com>
2018-11-15 14:30:59 -0500

    Add note command and multiple notes

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,30 @@
 #![feature(custom_attribute)]
-#[macro_use] extern crate serde_derive;
+#[macro_use]
+extern crate serde_derive;
 
 use chrono::{DateTime, Local};
-use std::{cmp::Ordering, collections::BinaryHeap};
-use structopt::StructOpt;
+use itertools::{EitherOrBoth, Itertools};
+use std::{
+    cmp::Ordering,
+    collections::BinaryHeap,
+    fmt::{self, Display, Write},
+    io,
+    iter::once,
+};
 
-#[derive(Debug, Clone, Deserialize, Serialize, PartialOrd, Eq, PartialEq)]
+#[derive(Default, Debug, Clone, Deserialize, Serialize, PartialOrd, Eq, PartialEq)]
 pub struct Entry {
-    #[serde(default)]
-    start: Option<DateTime<Local>>,
-    #[serde(default)]
-    stop: Option<DateTime<Local>>,
-    #[serde(default)]
-    goal: String,
-    #[serde(default)]
-    result: String,
-    #[serde(default)]
-    notes: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub start: Option<DateTime<Local>>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub stop: Option<DateTime<Local>>,
+    #[serde(default, skip_serializing_if = "String::is_empty")]
+    pub goal: String,
+    #[serde(default, skip_serializing_if = "String::is_empty")]
+    pub result: String,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
+    pub notes: Vec<String>,
 }
-
 
 impl Ord for Entry {
     fn cmp(&self, other: &Self) -> Ordering {
@@ -30,19 +36,100 @@ impl Ord for Entry {
     }
 }
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "timelog", author="")]
-pub enum Opt {
-    #[structopt(name = "print", author="", about = "Print all log entries")]
-    Print {
-    },
-    #[structopt(name = "add", author="", about = "Add a log entry")]
-    Add {
-    },
-    #[structopt(name = "start", author="", about = "Create a new log entry")]
-    Start {
-    },
-    #[structopt(name = "stop", author="", about = "Complete the most recent log entry")]
-    Stop {
-    },
+fn fmt_str_title_pad(text: &str, title: &str, pad: usize) -> Result<String, fmt::Error> {
+    let mut s = String::new();
+    let it = once(title).zip_longest(text.lines()).enumerate();
+    for (i, either_or_both) in it {
+        if i != 0 {
+            writeln!(s)?;
+        }
+
+        let (left, right) = match either_or_both {
+            EitherOrBoth::Both(left, right) => (left, right),
+            EitherOrBoth::Left(left) => (left, ""),
+            EitherOrBoth::Right(right) => ("", right),
+        };
+        write!(s, "{:<width$}", left, width = pad)?;
+        write!(s, "{}", right)?;
+    }
+
+    Ok(s)
+}
+
+fn fmt_option_title_pad<T: Display>(
+    text: &Option<T>,
+    title: &str,
+    pad: usize,
+) -> Result<String, fmt::Error> {
+    let mut s = String::new();
+    write!(s, "{:<width$}", title, width = pad)?;
+    if let Some(right) = text {
+        write!(s, "{}", right)?;
+    } else {
+        writeln!(s, "--")?;
+    }
+    Ok(s)
+}
+
+impl fmt::Display for Entry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let duration = if let (Some(start), Some(stop)) = (self.start, self.stop) {
+            Some(stop - start)
+        } else {
+            None
+        };
+
+        enum Data<'a> {
+            OpDt(Option<DateTime<Local>>),
+            OpSt(Option<String>),
+            St(&'a str),
+        };
+
+        let duration = duration.map(|x| format!("{}m{}s", x.num_minutes(), x.num_seconds()));
+        let mut to_output = vec![
+            ("Start Time:", Data::OpDt(self.start)),
+            ("Stop Time:", Data::OpDt(self.stop)),
+            ("Duration:", Data::OpSt(duration)),
+            ("Goal:", Data::St(&self.goal)),
+            ("Result:", Data::St(&self.result)),
+        ];
+        for note in &self.notes {
+            to_output.push(("Note:", Data::St(note)));
+        }
+
+        let pad = to_output.iter().map(|x| x.0.len()).max().unwrap() + 1;
+
+        for (i, (title, data)) in to_output.iter().enumerate() {
+            if i != 0 {
+                writeln!(f)?;
+            }
+
+            match data {
+                Data::OpDt(x) => write!(f, "{}", fmt_option_title_pad(x, title, pad)?)?,
+                Data::OpSt(x) => write!(f, "{}", fmt_option_title_pad(x, title, pad)?)?,
+                Data::St(x) => write!(f, "{}", fmt_str_title_pad(x, title, pad)?)?,
+            }
+        }
+
+        Ok(())
+    }
+}
+
+pub fn read_entries<R: io::Read>(
+    reader: Option<R>,
+) -> Result<BinaryHeap<Entry>, serde_json::Error> {
+    if let Some(reader) = reader {
+        Ok(serde_json::from_reader(reader)?)
+    } else {
+        Ok(BinaryHeap::default())
+    }
+}
+
+pub fn write_entries<W: io::Write>(
+    writer: W,
+    entries: BinaryHeap<Entry>,
+) -> Result<(), serde_json::Error> {
+    let entries = entries.into_sorted_vec();
+    serde_json::to_writer_pretty(writer, &entries)?;
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,115 @@
-use timelog::Opt;
+use chrono::Local;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufReader, BufWriter, Read},
+};
 use structopt::StructOpt;
+use timelog::{read_entries, write_entries, Entry};
 
-fn main() {
+type Result<T> = std::result::Result<T, Box<Error>>;
+const LOGNAME: &str = "log.json";
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "timelog", author = "")]
+pub enum Opt {
+    #[structopt(name = "print", author = "", about = "Print all log entries")]
+    Print {},
+    #[structopt(name = "start", author = "", about = "Create a new log entry")]
+    Start {},
+    #[structopt(name = "stop", author = "", about = "Complete the latest log entry")]
+    Stop {},
+    #[structopt(name = "note", author = "", about = "Add a note to the latest log entry")]
+    Note {},
+}
+
+fn main() -> Result<()> {
     let opt = Opt::from_args();
-    println!("{:?}", opt);
+
+    let reader = get_file_reader(LOGNAME)?;
+    let mut entries = read_entries(reader)?;
+
+    match opt {
+        Opt::Print {} => {
+            let entries = entries.into_sorted_vec();
+            for (i, e) in entries.iter().enumerate() {
+                if i != 0 {
+                    println!();
+                }
+                println!("{}", e);
+            }
+        }
+        Opt::Start {} => {
+            let start = Local::now();
+            let stdin = io::stdin();
+            let mut stdin = stdin.lock();
+            let mut buf = Vec::new();
+            stdin.read_to_end(&mut buf)?;
+            let goal = String::from_utf8(buf)?;
+
+            let new_entry = Entry {
+                start: Some(start),
+                goal,
+                ..Entry::default()
+            };
+            entries.push(new_entry);
+            let writer = get_file_writer(LOGNAME)?;
+            write_entries(writer, entries)?;
+        }
+        Opt::Stop {} => {
+            let stop = Local::now();
+            let mut last_entry = entries.pop().ok_or("NoneError")?;
+            if last_entry.stop.is_none() {
+                let stdin = io::stdin();
+                let mut stdin = stdin.lock();
+                let mut buf = Vec::new();
+                stdin.read_to_end(&mut buf)?;
+                let result = String::from_utf8(buf)?;
+
+                last_entry.stop = Some(stop);
+                last_entry.result = result;
+            } else {
+                Err("last entry was already completed")?;
+            }
+            entries.push(last_entry);
+
+            let writer = get_file_writer(LOGNAME)?;
+            write_entries(writer, entries)?;
+        }
+        Opt::Note {} => {
+            let mut last_entry = entries.pop().ok_or("NoneError")?;
+            let stdin = io::stdin();
+            let mut stdin = stdin.lock();
+            let mut buf = Vec::new();
+            stdin.read_to_end(&mut buf)?;
+            let note = String::from_utf8(buf)?;
+
+            last_entry.notes.push(note);
+            entries.push(last_entry);
+
+            let writer = get_file_writer(LOGNAME)?;
+            write_entries(writer, entries)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn get_file_reader(filename: &str) -> Result<Option<BufReader<File>>> {
+    let reader = File::open(filename);
+
+    if let Err(e) = reader {
+        if e.kind() == std::io::ErrorKind::NotFound {
+            Ok(None)
+        } else {
+            Err(e)?
+        }
+    } else {
+        Ok(Some(BufReader::new(reader?)))
+    }
+}
+
+fn get_file_writer(filename: &str) -> Result<BufWriter<File>> {
+    let writer = File::create(filename);
+    Ok(BufWriter::new(writer?))
 }
