16b627ddbd28522e68434db32ce51d60b3afff88
Justin Browne <jebdude89@gmail.com>
2018-11-20 12:51:57 -0500

    Add initial summary functionality

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use chrono::Local;
+use chrono::{Local, Duration, Datelike};
 use std::{
+    collections::HashMap,
     error::Error,
     fs::File,
     io::{self, BufReader, BufWriter, Read},
@@ -22,6 +23,15 @@ struct Opt {
 enum SubCommand {
     #[structopt(name = "print", author = "", about = "Print all log entries")]
     Print {},
+    #[structopt(name = "summary", author = "", about = "Summarize time over certain time periods")]
+    Summary {
+        #[structopt(short = "y", long = "yearly")]
+        yearly: bool,
+        #[structopt(short = "w", long = "weekly")]
+        weekly: bool,
+        #[structopt(short = "d", long = "daily")]
+        daily: bool,
+    },
     #[structopt(name = "start", author = "", about = "Create a new log entry")]
     Start {},
     #[structopt(name = "stop", author = "", about = "Complete the latest log entry")]
@@ -49,6 +59,36 @@ fn main() -> Result<()> {
                 }
                 println!("{}", e);
             }
+        }
+        SubCommand::Summary {..} => {
+            let mut yearly = HashMap::new();
+            let mut monthly = HashMap::new();
+            let mut weekly = HashMap::new();
+            let mut daily = HashMap::new();
+
+            for e in entries.iter() {
+                if let (Some(start), Some(stop)) = (e.start, e.stop) {
+                    let y = start.year();
+                    let m = start.month();
+                    let w = start.iso_week().week();
+                    let d = start.ordinal();
+                    let dur = stop - start;
+
+                    let entry = yearly.entry(y).or_insert(Duration::zero());
+                    *entry = *entry + dur;
+                    let entry = monthly.entry((y, m)).or_insert(Duration::zero());
+                    *entry = *entry + dur;
+                    let entry = weekly.entry((y, w)).or_insert(Duration::zero());
+                    *entry = *entry + dur;
+                    let entry = daily.entry((y, d)).or_insert(Duration::zero());
+                    *entry = *entry + dur;
+                }
+            }
+
+            println!("{:?}", yearly);
+            println!("{:?}", monthly);
+            println!("{:?}", weekly);
+            println!("{:?}", daily);
         }
         SubCommand::Start {} => {
             let start = Local::now();
